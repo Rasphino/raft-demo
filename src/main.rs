c61c03141a3cee984ae40526d804905079083909
Honghao Li <im.lihh@outlook.com>
2020-12-09 17:29:44 +0800

    Shit 2

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,19 @@ use anyhow::Result;
 use async_raft::{raft, AppData, AppDataResponse, Config, RaftError, RaftNetwork};
 use async_trait::async_trait;
 use memstore::{ClientRequest, ClientResponse, MemStore};
-use once_cell::sync::Lazy;
+use once_cell::sync::{Lazy, OnceCell};
 use tonic::{Code, Status};
 use tonic::codegen::Future;
 use crate::raft_rpc::raft_client::RaftClient;
+use tonic::transport::Channel;
+use crate::raft_rpc::AppendEntriesRequest;
+use async_raft::raft::AppendEntriesResponse;
+use std::cell::RefCell;
+use std::ops::Deref;
 
 const PORT_BASE: u64 = 50000;
 
-static RAFT_NODE: Lazy<Mutex<MemRaft>> = Lazy::new(|| {
-    println!("Initializing...");
-    let node_id = std::env::args().nth(1).expect("Expect node id").parse::<u64>().expect("Invalid node id");
-    let group_size = std::env::args().nth(2).expect("Expect group size").parse::<u64>().expect("Invalid group size");
-    println!("Node Id: {}, Group Size: {}", node_id, group_size);
-
-    let addr = format!("localhost:{}", PORT_BASE + node_id).parse().unwrap();
-    let rpc_server = RpcServer::default();
-    tokio::spawn(async move {
-        Server::builder()
-            .add_service(RaftServer::new(rpc_server))
-            .serve(addr).await.unwrap();
-    }).await.unwrap();
-
-    // TODO: initialize grpc server and clients and save them to `network`
-
-    let config = Arc::new(
-        Config::build("primary-raft-group".into())
-            .validate()
-            .expect("Failed to build raft config"),
-    );
-    let network = Arc::new(Network {});
-    let storage = Arc::new(MemStore::new(node_id));
-    Mutex::new(MemRaft::new(node_id, config, network, storage))
-});
+static RAFT_NODE: OnceCell<Mutex<MemRaft>> = OnceCell::new();
 
 // =======================
 // Application Data
@@ -72,8 +53,7 @@ static RAFT_NODE: Lazy<Mutex<MemRaft>> = Lazy::new(|| {
 // Network
 // =======================
 struct Network {
-    // TODO: save grpc clients and server
-    // client: Lazy<>
+    clients: Mutex<Vec<RaftClient<Channel>>>
 }
 
 #[async_raft::async_trait::async_trait]
@@ -84,7 +64,35 @@ impl RaftNetwork<memstore::ClientRequest> for Network {
         target: async_raft::NodeId,
         rpc: raft::AppendEntriesRequest<ClientRequest>,
     ) -> Result<raft::AppendEntriesResponse> {
-        unimplemented!()
+        let request = tonic::Request::new(AppendEntriesRequest {
+            term: rpc.term,
+            leader_id: rpc.leader_id,
+            prev_log_index: rpc.prev_log_index,
+            prev_log_term: rpc.prev_log_term,
+            entries: serde_json::to_string(&rpc.entries).unwrap_or("".to_string()),
+            leader_commit: rpc.leader_commit
+        });
+
+        let response = {
+            let mut lock = self.clients.lock().await;
+            let client = (*lock).get_mut(target as usize).unwrap();
+
+            client.append_entries(request).await
+        };
+
+        match response {
+            Ok(response) => {
+                let response = response.into_inner();
+                Ok(AppendEntriesResponse {
+                    term: (&response).term,
+                    success: (&response).success,
+                    conflict_opt: serde_json::from_str(&(&response).conflict_opt).ok()
+                })
+            },
+            Err(e) => {
+                Err(anyhow::Error::new(e))
+            }
+        }
     }
 
     async fn install_snapshot(
@@ -125,7 +133,7 @@ impl raft_rpc::raft_server::Raft for RpcServer {
         };
 
         let res = {
-            let raft = RAFT_NODE.lock().await;
+            let raft = RAFT_NODE.get().unwrap().lock().await;
             raft.append_entries(request).await
         };
         match res.map(|res| raft_rpc::AppendEntriesResponse {
@@ -154,7 +162,7 @@ impl raft_rpc::raft_server::Raft for RpcServer {
             done: (&request).done,
         };
         let res = {
-            let raft = RAFT_NODE.lock().await;
+            let raft = RAFT_NODE.get().unwrap().lock().await;
             raft.install_snapshot(request).await
         };
         match res.map(|res| raft_rpc::InstallSnapshotResponse { term: res.term }) {
@@ -174,7 +182,7 @@ impl raft_rpc::raft_server::Raft for RpcServer {
             last_log_index: (&request).last_log_index,
             last_log_term: (&request).last_log_term
         };
-        let res = RAFT_NODE.lock().await.vote(request).await;
+        let res = RAFT_NODE.get().unwrap().lock().await.vote(request).await;
         match res.map(|res| raft_rpc::VoteResponse { term: res.term, vote_granted: res.vote_granted })
         {
             Ok(r) => Ok(tonic::Response::new(r)),
@@ -187,7 +195,35 @@ type MemRaft = async_raft::Raft<ClientRequest, ClientResponse, Network, memstore
 
 #[tokio::main]
 async fn main() {
-    let mut client = RaftClient::connect("http://[::1]:50051").await?;
+    println!("Initializing...");
+    let node_id = std::env::args().nth(1).expect("Expect node id").parse::<u64>().expect("Invalid node id");
+    let group_size = std::env::args().nth(2).expect("Expect group size").parse::<u64>().expect("Invalid group size");
+    println!("Node Id: {}, Group Size: {}", node_id, group_size);
+
+    // start grpc server
+    let addr = format!("127.0.0.1:{}", PORT_BASE + node_id).parse().unwrap();
+    let rpc_server = RpcServer::default();
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(RaftServer::new(rpc_server))
+            .serve(addr).await.unwrap();
+    }).await.unwrap();
+
+    let mut clients = vec![];
+    for i in 1..=group_size {
+        let mut client = RaftClient::connect(format!("127.0.0.1:{}", PORT_BASE + i)).await.unwrap();
+        clients.push(client);
+    }
+
+    let config = Arc::new(
+        Config::build("primary-raft-group".into())
+            .validate()
+            .expect("Failed to build raft config"),
+    );
+    let network = Arc::new(Network { clients: Mutex::new(clients) });
+    let storage = Arc::new(MemStore::new(node_id));
+    let raft = Mutex::new(MemRaft::new(node_id, config, network, storage));
+    RAFT_NODE.set(raft);
 
     // TODO: What can we do with the raft node? How can we implement our business logic based on the provided raft api?
 }
